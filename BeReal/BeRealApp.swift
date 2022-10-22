aadfda2f848fad89de3b866356c8e2a0cb4bde83
Yannick Juarez <yannickjuarez@architech.agency>
2022-10-22 13:48:59 +0200

    Initial Commit

---
+++ BeReal/BeRealApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeRealApp.swift
+//  BeReal
+//
+//  Created by Yannick Juarez on 22/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct BeRealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
